f6ed6565144fd8db5016470f0fa0817ba7f96027
dyamid <dyamid@gmail.com>
2022-04-02 09:54:12 -0500

    feat: se crea componente dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', loadChildren: () => import('./components/inicio/inicio.module').then(m => m.InicioModule) },
+  { path: 'shop', loadChildren: () => import('./components/shop/shop.module').then(m => m.ShopModule) },
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
